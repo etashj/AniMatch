04093b607af42fc11ac7b0197eba2c015328cf5f
etashj <etashjhanji@gmail.com>
2025-06-21 14:26:06 -0400

    hover and active anims

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,25 @@ function App() {
 
   return (
     <>
-      <div id='header' className='flex flex-row flex-nowrap justify-between'>
-        <h1 className="font-header font-bold text-4xl ">AniMatch</h1>
-        <div id='btnCont' className="font-sans flex flex-row flow-nowrap justify-left">
-          <button className='text-zinc-200 bg-[#1D2126] px-4 rounded-2xl sm:rounded-2xl' onClick={() => setCount((count) => count + 1)}>
-            <div className='flex gap-2 flex-row flex-nowrap justify-between items-center'>
-              <p className='align-middle hidden sm:inline'>Sign in with AniList</p>
-              <img className='h-8 w-8 object-cover' src='src/assets/anilist.svg'></img>
-            </div>
-          </button>
-          <button className='bg-[#1D2126] px-1 mx-2 rounded-2xl sm:rounded-2xl' onClick={() => setVisible(true)}>
-            <div className='flex gap-2 flex-row flex-nowrap justify-between items-center'>
-              <img className='h-8 w-8 object-cover' src='src/assets/settings.svg'></img>
-            </div>
-          </button>
+      <div id='mainCont' className={`${settingsVisible? 'blur-xs' : ''} transition duration-300`}>
+        <div id='header' className='flex flex-row flex-nowrap justify-between'>
+          <h1 className="font-header font-bold text-4xl ">AniMatch</h1>
+          <div id='btnCont' className="font-sans flex flex-row flow-nowrap justify-left">
+            <button className='text-zinc-200 bg-[#1D2126] px-4 rounded-2xl sm:rounded-2xl hover:shadow-xl/20 transition duration-300 active:shadow-md/40' onClick={() => setCount((count) => count + 1)}>
+              <div className='flex gap-2 flex-row flex-nowrap justify-between items-center'>
+                <p className='font-bold align-middle hidden sm:inline'>Sign in with AniList</p>
+                <img className='h-8 w-8 object-cover' src='src/assets/anilist.svg'></img>
+              </div>
+            </button>
+            <button className='bg-[#1D2126] px-1 mx-2 rounded-2xl sm:rounded-2xl hover:shadow-xl/20 transition duration-300 active:shadow-md/40' onClick={() => setTimeout(() => {setVisible(true);}, 100)}>
+              <div className='flex gap-2 flex-row flex-nowrap justify-between items-center'>
+                <img className='h-8 w-8 object-cover' src='src/assets/settings.svg'></img>
+              </div>
+            </button>
+          </div>
         </div>
       </div>
-      <Settings visible={settingsVisible} onClose={() => setVisible(false)} />
+      <Settings visible={settingsVisible} onClose={() => setTimeout(() => {setVisible(false);}, 100)}/>
     </>
   )
 }
--- src/settingsModal.tsx
+++ src/settingsModal.tsx
@@ -9,7 +9,7 @@ function Settings( {visible, onClose} : Props ) {
   return (
     <div className="bg-zinc-200 px-2 py-2 rounded-3xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed flex-column flex-nowrap justify-between space-y-2">
       <p className='mx-3 font-bold'>Settings</p>
-      <button onClick={onClose} className='w-full font-bold text-zinc-200 bg-[#1D2126] px-2 py-1 rounded-2xl'>close</button>
+      <button onClick={onClose} className='w-full font-bold text-zinc-200 bg-[#1D2126] px-2 py-1 rounded-2xl hover:shadow-xl/20 transition duration-300 active:shadow-md/40'>close</button>
     </div>
   );
 }
