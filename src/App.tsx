dd59b9849182ff517b803c4c6dda5ea4eb259798
etashj <etashjhanji@gmail.com>
2025-06-21 14:12:10 -0400

    responsivity for buttons, settings modal works

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,27 @@ import Settings from './settingsModal.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [settingsVisible, setVisible] = useState(false)
 
   return (
     <>
       <div id='header' className='flex flex-row flex-nowrap justify-between'>
-        <h1 className="font-header font-bold text-2xl lg:text-4xl">AniMatch</h1>
+        <h1 className="font-header font-bold text-4xl ">AniMatch</h1>
         <div id='btnCont' className="font-sans flex flex-row flow-nowrap justify-left">
-          <button className='text-zinc-200 bg-[#1D2126] px-4 rounded-xl' onClick={() => setCount((count) => count + 1)}>
+          <button className='text-zinc-200 bg-[#1D2126] px-4 rounded-2xl sm:rounded-2xl' onClick={() => setCount((count) => count + 1)}>
             <div className='flex gap-2 flex-row flex-nowrap justify-between items-center'>
-              <p className='align-middle'>Sign in with AniList</p>
+              <p className='align-middle hidden sm:inline'>Sign in with AniList</p>
               <img className='h-8 w-8 object-cover' src='src/assets/anilist.svg'></img>
             </div>
           </button>
-          <button className='bg-[#1D2126] px-1 mx-2 rounded-xl' onClick={() => setCount((count) => count + 1)}>
+          <button className='bg-[#1D2126] px-1 mx-2 rounded-2xl sm:rounded-2xl' onClick={() => setVisible(true)}>
             <div className='flex gap-2 flex-row flex-nowrap justify-between items-center'>
               <img className='h-8 w-8 object-cover' src='src/assets/settings.svg'></img>
             </div>
           </button>
         </div>
       </div>
-      <Settings />
+      <Settings visible={settingsVisible} onClose={() => setVisible(false)} />
     </>
   )
 }
--- src/settingsModal.tsx
+++ src/settingsModal.tsx
@@ -1,6 +1,16 @@
-function Settings() {
+type Props = {
+  visible: boolean;
+  onClose: () => void;
+};
+
+function Settings( {visible, onClose} : Props ) {
+  if (!visible) return null;
+  
   return (
-    <div className="fixed hidden">I am the settings modal</div>
+    <div className="bg-zinc-200 px-2 py-2 rounded-3xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed flex-column flex-nowrap justify-between space-y-2">
+      <p className='mx-3 font-bold'>Settings</p>
+      <button onClick={onClose} className='w-full font-bold text-zinc-200 bg-[#1D2126] px-2 py-1 rounded-2xl'>close</button>
+    </div>
   );
 }
 
