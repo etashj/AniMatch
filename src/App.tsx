5b249c5c809d1b8b4008b2aa40735e23032948d5
etashj <etashjhanji@gmail.com>
2025-06-21 23:05:21 -0400

    tigthened up border raidii and color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,15 @@ function App() {
     <>
       <div id='mainCont' className={`${settingsVisible? 'blur-xs' : ''} transition duration-300`}>
         <div id='header' className='flex flex-row flex-nowrap justify-between'>
-          <h1 className="font-header font-bold text-4xl ">AniMatch</h1>
+          <h1 className="font-header font-bold text-4xl dark:text-zinc-200">AniMatch</h1>
           <div id='btnCont' className="font-sans flex flex-row flow-nowrap justify-left">
-            <button className='text-zinc-200 bg-[#1D2126] px-4 rounded-2xl sm:rounded-2xl hover:shadow-xl/20 transition duration-300 active:shadow-md/40' onClick={() => setCount((count) => count + 1)}>
+            <button className='text-zinc-200 bg-[#1D2126] dark:bg-[#24272a] px-4 rounded-2xl sm:rounded-2xl hover:shadow-xl/20 dark:hover:shadow-indigo-500 transition duration-300 active:shadow-md/40' onClick={() => setCount((count) => count + 1)}>
               <div className='flex gap-2 flex-row flex-nowrap justify-between items-center'>
                 <p className='font-bold align-middle hidden sm:inline'>Sign in with AniList</p>
                 <img className='h-8 w-8 object-cover' src='src/assets/anilist.svg'></img>
               </div>
             </button>
-            <button className='bg-[#1D2126] px-1 mx-2 rounded-2xl sm:rounded-2xl hover:shadow-xl/20 transition duration-300 active:shadow-md/40' onClick={() => setTimeout(() => {setVisible(true);}, 100)}>
+            <button className='bg-indigo-500 px-1 mx-2 rounded-2xl sm:rounded-2xl hover:shadow-xl/20 transition duration-300 active:shadow-md/40 dark:hover:shadow-indigo-500 ' onClick={() => setTimeout(() => {setVisible(true);}, 100)}>
               <div className='flex gap-2 flex-row flex-nowrap justify-between items-center'>
                 <img className='h-8 w-8 object-cover' src='src/assets/settings.svg'></img>
               </div>
--- src/settingsModal.tsx
+++ src/settingsModal.tsx
@@ -7,9 +7,9 @@ function Settings( {visible, onClose} : Props ) {
   if (!visible) return null;
   
   return (
-    <div className="bg-zinc-200 px-2 py-2 rounded-3xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed flex-column flex-nowrap justify-between space-y-2">
-      <p className='mx-3 font-bold'>Settings</p>
-      <button onClick={onClose} className='w-full font-bold text-zinc-200 bg-[#1D2126] px-2 py-1 rounded-2xl hover:shadow-xl/20 transition duration-300 active:shadow-md/40'>close</button>
+    <div className="bg-zinc-200 dark:bg-[#141112] px-2 py-2 rounded-3xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed flex-column flex-nowrap justify-between space-y-2">
+      <p className='mx-3 font-bold dark:text-zinc-200'>Settings</p>
+      <button onClick={onClose} className='w-full font-bold text-zinc-200 bg-indigo-500 px-2 py-1 rounded-b-2xl rounded-t-md hover:shadow-md/20 transition duration-300 active:shadow-sm/40 dark:hover:shadow-indigo-500'>close</button>
     </div>
   );
 }
